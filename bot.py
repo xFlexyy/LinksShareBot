0409237652e1f0fc72d7ab6a00a7acac6fc908fa
dByte Flexyy <officialzeno444@gmail.com>
2025-09-09 01:40:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ class Bot(Client):
             self.LOGGER(__name__).warning(f"Failed to notify owner ({OWNER_ID}) of bot start: {e}")
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info("Bot Running..!\n\nCreated by \nhttps://t.me/ProObito")
+        self.LOGGER(__name__).info("Bot Running..!\n\nCreated by \nhttps://t.me/xFlexyy")
         self.LOGGER(__name__).info(f"{name}")
         self.username = usr_bot_me.username
 
